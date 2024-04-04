ad9aacd0deaa6cf5626d474da755ef56a9dc1b39
Doomtower <jessehudak707@outlook.com>
2024-04-04 16:30:25 -0700

    changing base in vite.config from /JHDKportfolio/ to './'

--- JHDKportfolio/vite.config.js
+++ JHDKportfolio/vite.config.js
@@ -8,7 +8,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/JHDKportfolio/',
+  base: './',
   plugins: [
     vue(),
     vueJsx(),
